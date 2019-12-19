7e08cb6b07caefc6e1065f0b5bd15f48a5760877
chenzhanzhan <963076250@qq.com>
2019-12-19 20:32:44 +0800

    登录模块-登录成功及失败处理

--- src/main.js
+++ src/main.js
@@ -5,8 +5,9 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
 import axios from 'axios'
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios
-axios.defaults.baseURL = 'http">http://ttapi.research.itcast.cn/mp/v1_0'
+
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -5,11 +5,11 @@
 <img src="../../assets/img/logo_index.png" alt="">
 </div>
     <el-form ref='myform' :model="rulesForm" :rules="rules">
-        <el-form-item   prop="PhoneNumber">
-          <el-input v-model='rulesForm.PhoneNumber' placeholder="请输入手机号"></el-input>
+        <el-form-item   prop="mobile">
+          <el-input v-model='rulesForm.mobile' placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item prop='IDCode'>
-          <el-input v-model='rulesForm.IDCode' class="ipt" placeholder="请输入验证码"></el-input><el-button style="float:right" plain >发送验证码</el-button>
+        <el-form-item prop='code'>
+          <el-input v-model='rulesForm.code' class="ipt" placeholder="请输入验证码"></el-input><el-button style="float:right" plain >发送验证码</el-button>
         </el-form-item>
         <el-form-item  prop='agreement'>
         <el-checkbox v-model='rulesForm.agreement'>我已阅读并同意用户协议和隐私条款</el-checkbox>
@@ -30,13 +30,13 @@ export default {
     }
     return {
       rulesForm: {
-        PhoneNumber: '',
-        IDCode: '',
+        mobile: '',
+        code: '',
         agreement: false
       },
       rules: {
-        PhoneNumber: [{ required: true, message: '手机号不能为空' }, { pattern: /^1[3456789]\d{9}$/, message: '请输入合法手机号' }],
-        IDCode: [{ required: true, message: '验证码不能为空' }, { pattern: /^\d{6}$/, message: '验证码数字长度为6位' }],
+        mobile: [{ required: true, message: '手机号不能为空' }, { pattern: /^1[3456789]\d{9}$/, message: '请输入合法手机号' }],
+        code: [{ required: true, message: '验证码不能为空' }, { pattern: /^\d{6}$/, message: '验证码数字长度为6位' }],
         agreement: [{ validator }]
       }
     }
@@ -45,7 +45,19 @@ export default {
     submitForm () {
       this.$refs.myform.validate((value) => {
         if (value) {
-          alert('执行下面的东西')
+          this.$axios.post('/authorizations', this.rulesForm).then(res => {
+            //  then中只接受正确时返回的数据
+            localStorage.setItem('user-token', res.data.data.token)
+            // 将token存储到本地,再跳转到主页（登录页面）
+            this.$router.push('/home')
+          }).catch(() => {
+            // catch中只接受错误时elementui中有一个提示信息插件
+            this.$message({
+              type: 'warning',
+              message: '手机号或验证码错误'
+
+            })
+          })
         }
       })
     }
