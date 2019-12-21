6a5b201894a0ec8f72e0709afd3fd3d6088e07c1
chenzhanzhan <963076250@qq.com>
2019-12-21 20:05:35 +0800

    主页模块-导航守卫

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './permission/index'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './styles/index.less'
---
+++ src/permission/index.js
@@ -0,0 +1,14 @@
+// 处理路由拦截器（导航守卫）
+import router from '../router/index'
+router.beforeEach(function (to, from, next) {
+  if (to.path.startsWith('/home')) {
+    let token = window.localStorage.getItem('user-token')
+    if (token) {
+      next()
+    } else {
+      next('/login')
+    }
+  } else {
+    next()
+  }
+})
