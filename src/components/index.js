31213343bd7fa06e8d27d59c999890019094c22c
chenzhanzhan <963076250@qq.com>
2019-12-22 09:08:43 +0800

    面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import layoutAside from './home/layoutAside.vue'
 import layoutheader from './home/layoutheader.vue'
+import breadcrumb from './breadcrumb/breadcrumb.vue'
 
 export default {
 
   install (Vue) {
     Vue.component('layoutAside', layoutAside)
     Vue.component('layoutheader', layoutheader)
+    Vue.component('breadcrumb', breadcrumb)
   }
 
 }
