9dcbad7d06f50aec153018bbeb0247edf27f4a64
chenzhanzhan <963076250@qq.com>
2019-12-22 09:10:09 +0800

    面包屑组件的封装2

---
+++ src/components/breadcrumb/breadcrumb.vue
@@ -0,0 +1,18 @@
+<template>
+<el-breadcrumb separator=">">
+    <el-breadceumb-item to='/home'>首页</el-breadceumb-item>
+    <el-breadceumb-item>
+        <slot name='title'> </slot>
+    </el-breadceumb-item>
+</el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
