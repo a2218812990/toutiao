e07a19ad86524140a63751fcff70f203c1d02529
chenzhanzhan <963076250@qq.com>
2019-12-30 10:58:19 +0800

    页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,6 +1,9 @@
 // 处理路由拦截器（导航守卫）
 import router from '../router/index'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 router.beforeEach(function (to, from, next) {
+  NProgress.start()
   if (to.path.startsWith('/home')) {
     let token = window.localStorage.getItem('user-token')
     if (token) {
@@ -11,4 +14,7 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+router.afterEach(function () {
+  NProgress.done()
 })
